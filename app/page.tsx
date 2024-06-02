2bfd627be64c34b79098bc8cbbee6cf42f3ffe61
Mossa <mossa708496@gmail.com>
2024-06-02 12:30:01 +0300

    added per-user data isolation

--- app/page.tsx
+++ app/page.tsx
@@ -43,6 +43,7 @@ export default function App() {
       {({ user, signOut }) => (
         <main>
           <h1>My todos</h1>
+          <h1>{user?.signInDetails?.loginId}'s todos</h1>
           <button onClick={createTodo}>+ new</button>
           <ul>
             {todos.map((todo) => (
